f85382a0072be511b2699d0ebab195ee65fd711b
IvanLubnin <vanya.lubnin@gmail.com>
2025-11-12 14:57:34 +0200

    added backend for login/reg logic

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -25,7 +25,7 @@ export default function AuthPage() {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:4000/api/login", {
+      const res = await fetch("http://localhost:3000/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(loginData),
